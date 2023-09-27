5d959f76850b470b5f062251343f43430961ac80
eduardo <real.777.em@gmail.com>
2023-09-26 20:34:13 -0500

    hola

---
+++ semana09/dia01-react-intro/src/App.jsx
@@ -0,0 +1,21 @@
+import Counter from'./counter'
+import './App.css'
+
+
+
+function App() {
+  
+  
+  return (
+    <>
+    <h1>counter</h1>
+  
+
+    <Counter iniciador={4}/>
+    <Counter />
+   
+    </>
+  )
+}
+
+export default App
